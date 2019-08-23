19c00bc4044aef9f40f6bd1157ae90f0780e8d85
Shinnosuke Sawada <6warashi9@gmail.com>
2019-08-23 21:21:30 +0900

    add implements

---
+++ semaphore.go
@@ -0,0 +1,84 @@
+package semaphore // import "github.com/Warashi/go-semaphore"
+
+import (
+	"context"
+	"sync"
+	"sync/atomic"
+)
+
+type Semaphore interface {
+	Acquire()
+	Release()
+	IncreaseSize(n int64) int64
+}
+
+var _ Semaphore = &NonFairSemaphore{}
+
+type NonFairSemaphore struct {
+	size int64
+	cur  int64
+	cond *sync.Cond
+}
+
+func NewNonFair(size int64) *NonFairSemaphore {
+	return &NonFairSemaphore{
+		size: size,
+		cur:  0,
+		cond: sync.NewCond(new(sync.Mutex)),
+	}
+}
+
+func (s *NonFairSemaphore) Acquire() {
+	for {
+		if atomic.AddInt64(&s.cur, 1) <= atomic.LoadInt64(&s.size) {
+			return
+		}
+		s.cond.L.Lock()
+		atomic.AddInt64(&s.cur, -1)
+		for atomic.LoadInt64(&s.size) <= atomic.LoadInt64(&s.cur) {
+			s.cond.Wait()
+		}
+		s.cond.L.Unlock()
+	}
+}
+
+func (s *NonFairSemaphore) Release() {
+	s.cond.L.Lock()
+	if atomic.AddInt64(&s.cur, -1) < 0 {
+		panic("released semaphore more than acquired")
+	}
+	s.cond.Signal()
+	s.cond.L.Unlock()
+}
+
+func (s *NonFairSemaphore) TryAcquire(ctx context.Context) error {
+	got := make(chan struct{})
+	go func() {
+		s.Acquire()
+		got <- struct{}{}
+	}()
+	select {
+	case <-got:
+		return nil
+	case <-ctx.Done():
+		go func() {
+			<-got
+			s.Release()
+		}()
+		return ctx.Err()
+	}
+}
+
+func (s *NonFairSemaphore) IncreaseSize(n int64) int64 {
+	s.cond.L.Lock()
+	newSize := atomic.AddInt64(&s.size, n)
+	if 1 <= newSize {
+		s.cond.Broadcast()
+		s.cond.L.Unlock()
+		return newSize
+	}
+	atomic.StoreInt64(&s.size, 1)
+	s.cond.Broadcast()
+	s.cond.L.Unlock()
+	return 1
+}
---
+++ semaphore_test.go
@@ -0,0 +1,107 @@
+package semaphore_test
+
+import (
+	"sync"
+	"sync/atomic"
+	"testing"
+
+	"github.com/Warashi/go-semaphore"
+)
+
+
+func TestSemaphore_Release(t *testing.T) {
+	s := semaphore.NewNonFair(1)
+
+	var wg sync.WaitGroup
+	var c int64
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer s.Release()
+			defer wg.Done()
+			s.Acquire()
+			atomic.AddInt64(&c, 1)
+		}()
+	}
+	wg.Wait()
+	if got := atomic.LoadInt64(&c); got != 10 {
+		t.Errorf("expected %d, got %d", 10, got)
+	}
+}
+
+func TestSemaphore_IncreaseSize(t *testing.T) {
+	s := semaphore.NewNonFair(0)
+
+	var wg sync.WaitGroup
+	var c int64
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			s.Acquire()
+			atomic.AddInt64(&c, 1)
+		}()
+	}
+	for i := 0; i < 10; i++ {
+		s.IncreaseSize(1)
+	}
+	wg.Wait()
+	if got := atomic.LoadInt64(&c); got != 10 {
+		t.Errorf("expected %d, got %d", 10, got)
+	}
+}
+
+func TestSemaphore_IncreaseSizeBulk(t *testing.T) {
+	s := semaphore.NewNonFair(0)
+
+	var wg sync.WaitGroup
+	var c int64
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			s.Acquire()
+			atomic.AddInt64(&c, 1)
+		}()
+	}
+	s.IncreaseSize(10)
+	wg.Wait()
+	if got := atomic.LoadInt64(&c); got != 10 {
+		t.Errorf("expected %d, got %d", 10, got)
+	}
+}
+
+func BenchmarkSemaphore(b *testing.B) {
+	const size = 10
+	s := semaphore.NewNonFair(size)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			s.Acquire()
+			s.Release()
+		}
+	})
+}
+
+func BenchmarkSemaphore_IncreaseSize(b *testing.B) {
+	const size = 10
+	s := semaphore.NewNonFair(size)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			s.Acquire()
+			s.IncreaseSize(1)
+		}
+	})
+}
+
+func BenchmarkSemaphore_Release_IncreaseSize(b *testing.B) {
+	const size = 10
+	s := semaphore.NewNonFair(size)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			s.Acquire()
+			s.IncreaseSize(1)
+			s.Acquire()
+			s.Release()
+		}
+	})
+}
